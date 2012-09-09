00537a79749d2ed82a08bb2ee58910c05a1ced3f
k-u12 <k@u12.(none)>
2012-09-09 23:51:06 +1000

    init-commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trainbuddy::Application.config.secret_token = '541e7a96297152f094294255d2c715e03bbf03415a5a4612cc54ae7d559cd362123ed43b37cea8bbdf37a846bb871fb3fc8f82828089089225515aa316b80d11'
