0c8601f6bc08e11e98019d4fef0808b7e86e4ed0
k-u12 <k@u12.(none)>
2012-09-10 13:52:41 +1000

    chap-4 end

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def site_title(sub_title)
+    sub_title.nil? ? "Trainbuddy" : "Trainbuddy | #{sub_title}"
+  end
+  
+  def logo
+    logo = image_tag("/trainbuddy/public/images/clay.jpeg", :alt=>"trainbuddy", :class=>"logo", :size=>"210x80" )
+  end
+
 end
